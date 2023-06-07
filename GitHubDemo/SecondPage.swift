ad36743376f4f6ba6ded19b428bb17d5d8e7573d
matikos <mati.kos@me.com>
2023-06-07 22:39:47 +0100

    Added Second Page

---
+++ GitHubDemo/SecondPage.swift
@@ -0,0 +1,20 @@
+//
+//  SecondPage.swift
+//  GitHubDemo
+//
+//  Created by Your Host on 6/7/23.
+//
+
+import SwiftUI
+
+struct SecondPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondPage_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondPage()
+    }
+}
